a3fec492e28379499262c23c4eb1006b5709c4c1
delavina <34104127+delavina@users.noreply.github.com>
2023-12-16 20:45:35 +0100

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant='destructive' className='mx-12'>Hello</Button>
+    </main>
+  )
+}
