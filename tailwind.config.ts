687b5a28ac26ab0d0ab364d7002c43d5555f5948
delavina <34104127+delavina@users.noreply.github.com>
2023-12-20 10:14:42 +0100

    color update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,9 +20,9 @@ module.exports = withUt({
     extend: {
       colors: {
         primary: {
-          500: '#624CF5',
-          50: ' #F6F8FD',
-          DEFAULT: '#624CF5',
+          500: '#949292', /* ersetzte Farbe: #624cf5 */
+          50: ' #EAE9E9', /* ersetzte Farbe: #F6F8FD */
+          DEFAULT: '#949292', /* ersetzte Farbe: #624cf5 */
           foreground: 'hsl(var(--primary-foreground))',
         },
         coral: {
